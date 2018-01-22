d39772cf0e5b9f0830733d90239dd00c3af4024c
Josh West <joshw@zillowgroup.com>
2018-01-22 15:53:51 -0800

    updating files

--- js/app.js
+++ js/app.js
@@ -3,17 +3,18 @@
 // array to store the image instances
 Photo.allPhotos = [];
 
+// global variable
+
 // constructor for the image objects
-function Photo(filepath, name){
+function Photo(filepath, name, timesCicked){
   this.filepath = filepath;
   this.name = name;
-  var timesClicked = 0;
-  var timeShown = 0;
+  this.timesClicked = timesClicked;
   Photo.allPhotos.push(this);
 }
 
 // Create instances of images
-new Photo('img/bag.jpg', 'Bag');
+new Photo('img/bag.jpg', 'Bag', 0);
 new Photo('img/banana.jpg', 'Banana');
 new Photo('img/bathroom.jpg', 'Bathroom');
 new Photo('img/boots.jpg', 'Boots');
@@ -35,10 +36,15 @@ new Photo('img/water-can.jpg', 'Water');
 new Photo('img/wine-glass.jpg', 'Wine Glass');
 
 // access the image from the DOM
-var imgEl = document.getElementById('photos');
+var imgEl = document.getElementById('photos1');
+var img2El = document.getElementById('photos2');
+var img3El = document.getElementById('photos3');
 
 // add event listener
 imgEl.addEventListener('click', randomPhoto);
+img2El.addEventListener('click', randomPhoto2);
+img3El.addEventListener('click', randomPhoto3);
+
 
 // callback function to diaplay random image
 function randomPhoto() {
@@ -49,5 +55,24 @@ function randomPhoto() {
   imgEl.src = Photo.allPhotos[randomIndex].filepath;
 }
 
-// invoke the callback on page load to show random phtos
+function randomPhoto2() {
+  // random number generator to return a number between 0 and length of the array. (Photo.allPhotos)
+  var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
+
+  // use the random number to diaplay photos at the random index
+  img2El.src = Photo.allPhotos[randomIndex].filepath;
+}
+
+function randomPhoto3() {
+  // random number generator to return a number between 0 and length of the array. (Photo.allPhotos)
+  var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
+
+  // use the random number to diaplay photos at the random index
+  img3El.src = Photo.allPhotos[randomIndex].filepath;
+}
+
+// invoke the callback on page load to show random photos
 randomPhoto();
+randomPhoto2();
+randomPhoto3();
+
