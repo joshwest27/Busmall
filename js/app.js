53c4d916c035b5e80f4ed7897fab8ab7f1cb6da1
Josh West <joshw@zillowgroup.com>
2018-01-22 16:17:10 -0800

    wrapping all in random function

--- js/app.js
+++ js/app.js
@@ -6,15 +6,14 @@ Photo.allPhotos = [];
 // global variable
 
 // constructor for the image objects
-function Photo(filepath, name, timesCicked){
+function Photo(filepath, name){
   this.filepath = filepath;
   this.name = name;
-  this.timesClicked = timesClicked;
   Photo.allPhotos.push(this);
 }
 
 // Create instances of images
-new Photo('img/bag.jpg', 'Bag', 0);
+new Photo('img/bag.jpg', 'Bag');
 new Photo('img/banana.jpg', 'Banana');
 new Photo('img/bathroom.jpg', 'Bathroom');
 new Photo('img/boots.jpg', 'Boots');
@@ -41,38 +40,44 @@ var img2El = document.getElementById('photos2');
 var img3El = document.getElementById('photos3');
 
 // add event listener
-imgEl.addEventListener('click', randomPhoto);
-img2El.addEventListener('click', randomPhoto2);
-img3El.addEventListener('click', randomPhoto3);
+imgEl.addEventListener('click', allRandomPhotos);
+img2El.addEventListener('click', allRandomPhotos);
+img3El.addEventListener('click', allRandomPhotos);
 
 
+function allRandomPhotos (){
 // callback function to diaplay random image
-function randomPhoto() {
+  function randomPhoto() {
   // random number generator to return a number between 0 and length of the array. (Photo.allPhotos)
-  var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
+    var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
 
-  // use the random number to diaplay photos at the random index
-  imgEl.src = Photo.allPhotos[randomIndex].filepath;
-}
+    // use the random number to diaplay photos at the random index
+    imgEl.src = Photo.allPhotos[randomIndex].filepath;
+  }
 
-function randomPhoto2() {
+  function randomPhoto2() {
   // random number generator to return a number between 0 and length of the array. (Photo.allPhotos)
-  var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
+    var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
 
-  // use the random number to diaplay photos at the random index
-  img2El.src = Photo.allPhotos[randomIndex].filepath;
-}
+    // use the random number to diaplay photos at the random index
+    img2El.src = Photo.allPhotos[randomIndex].filepath;
+  }
 
-function randomPhoto3() {
+  function randomPhoto3() {
   // random number generator to return a number between 0 and length of the array. (Photo.allPhotos)
-  var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
+    var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
 
-  // use the random number to diaplay photos at the random index
-  img3El.src = Photo.allPhotos[randomIndex].filepath;
+    // use the random number to diaplay photos at the random index
+    img3El.src = Photo.allPhotos[randomIndex].filepath;
+  }
+  randomPhoto();
+  randomPhoto2();
+  randomPhoto3();
 }
 
 // invoke the callback on page load to show random photos
-randomPhoto();
-randomPhoto2();
-randomPhoto3();
+// randomPhoto();
+// randomPhoto2();
+// randomPhoto3();
+allRandomPhotos();
 
